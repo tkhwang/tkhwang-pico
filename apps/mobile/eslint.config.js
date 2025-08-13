3818d0a7f7caa5e1319f40b51d2c02fdff657df0
tkhwang <tkhwang@gmail.com>
2025-08-13 17:24:41 +0900

    feat(mobile): Enable import sorting in ESLint config

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -3,9 +3,21 @@ const expoConfig = require('eslint-config-expo/flat');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
-    expoConfig,
-    eslintPluginPrettierRecommended,
-    {
-        ignores: ['dist/*'],
+  expoConfig,
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist/*'],
+    rules: {
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false,
+        },
+      ],
     },
+  },
 ]);
