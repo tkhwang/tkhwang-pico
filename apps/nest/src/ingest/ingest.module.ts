1598dafff21c71e77447a6e3f08c980ce2cd7d6f
tkhwang <tkhwang@gmail.com>
2025-09-04 17:37:39 +0900

    🔧 chore(nest/embedding): add IngestEmbeddingService to IngestModule providers and exports

--- apps/nest/src/ingest/ingest.module.ts
+++ apps/nest/src/ingest/ingest.module.ts
@@ -3,10 +3,21 @@ import { IngestService } from './ingest.service';
 import { IngestSummaryService } from './ingest-summary.service';
 import { IngestExtractService } from './ingest-extract.service';
 import { SupabaseModule } from '../supabase/supabase.module';
+import { IngestEmbeddingService } from './ingest-embedding.service';
 
 @Module({
   imports: [SupabaseModule],
-  providers: [IngestService, IngestSummaryService, IngestExtractService],
-  exports: [IngestService, IngestSummaryService, IngestExtractService],
+  providers: [
+    IngestService,
+    IngestSummaryService,
+    IngestExtractService,
+    IngestEmbeddingService,
+  ],
+  exports: [
+    IngestService,
+    IngestSummaryService,
+    IngestExtractService,
+    IngestEmbeddingService,
+  ],
 })
 export class IngestModule {}
