d01122fdb0918aceb6ca0d1084f6684ba68f965a
tkhwang <tkhwang@gmail.com>
2025-09-23 10:27:44 +0900

    🔧 chore(eslint): standardize import statements and update ESLint rules for consistency

--- packages/config-eslint-prettier/eslint/flat/base.mjs
+++ packages/config-eslint-prettier/eslint/flat/base.mjs
@@ -1,9 +1,9 @@
-import eslint from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-import prettierPlugin from 'eslint-plugin-prettier';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 const baseConfig = tseslint.config(
   eslint.configs.recommended,
@@ -16,32 +16,36 @@ const baseConfig = tseslint.config(
       },
       parserOptions: {
         ecmaVersion: 2021,
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     plugins: {
       import: importPlugin,
       prettier: prettierPlugin,
-      'simple-import-sort': simpleImportSort,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
-      'import/order': 'off',
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      "import/order": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
       ],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
         },
       ],
-      'prettier/prettier': 'warn',
+      "@typescript-eslint/no-empty-interface": [
+        "error",
+        { allowSingleExtends: true },
+      ],
+      "prettier/prettier": "warn",
     },
-  },
+  }
 );
 
 export default baseConfig;
