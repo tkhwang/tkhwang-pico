5d9703dea4fd17b2afbfb37abac7e81ea51abf93
tkhwang <tkhwang@gmail.com>
2025-09-23 17:00:41 +0900

    🔧 config(web): add Prettier configuration and standardize ESLint rules

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,13 +1,13 @@
-import baseConfig from '@tkhwang-pico/config-eslint-prettier/eslint/next';
-import queryPlugin from '@tanstack/eslint-plugin-query';
+import queryPlugin from "@tanstack/eslint-plugin-query";
+import baseConfig from "@tkhwang-pico/config-eslint-prettier/eslint/next";
 
 const eslintConfig = [
   ...baseConfig,
   {
-    plugins: { '@tanstack/query': queryPlugin },
+    plugins: { "@tanstack/query": queryPlugin },
     rules: {
-      '@tanstack/query/exhaustive-deps': 'warn',
-      '@tanstack/query/no-rest-destructuring': 'off',
+      "@tanstack/query/exhaustive-deps": "warn",
+      "@tanstack/query/no-rest-destructuring": "off",
     },
   },
 ];
--- packages/config-eslint-prettier/prettier/prettier.base.cjs
+++ packages/config-eslint-prettier/prettier/prettier.base.cjs
@@ -2,7 +2,7 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: 'es5',
+  trailingComma: "all",
   bracketSameLine: false,
   jsxSingleQuote: false,
 };
