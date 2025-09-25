be595812a5afd88919316a6d7917e09117e4cd72
tkhwang <tkhwang@gmail.com>
2025-09-25 10:34:40 +0900

    🦄 refactor(web/supabase): update import path for Supabase client and create new service file

--- apps/web/lib/supabase/messages.ts
+++ apps/web/lib/supabase/messages.ts
@@ -5,7 +5,7 @@ import type {
 } from '@tkhwang-pico/supabase';
 import { MessagesRepository } from '@tkhwang-pico/supabase';
 
-import { createAuthenticatedSupabaseClient } from '../../services/supabase-client';
+import { createAuthenticatedSupabaseClient } from '../../services/supabase/supabase-client';
 import type { AuthClerkSession } from '../../types/auth';
 
 function createMessagesRepository(session: AuthClerkSession) {
