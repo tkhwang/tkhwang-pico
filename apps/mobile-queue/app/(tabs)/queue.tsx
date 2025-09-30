e3f4facff91b3d5832ab0eec7c2ad9206a3c0cfe
tkhwang <tkhwang@gmail.com>
2025-09-30 10:07:48 +0900

    🦄 refactor(mobile/queue): add Header component to QueueScreen and disable default header display in MainLayout

--- apps/mobile-queue/app/(tabs)/queue.tsx
+++ apps/mobile-queue/app/(tabs)/queue.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@/components/header';
 import { MainLayout } from '@/components/main-layout';
 import { Queue } from '@/components/queue';
 
 export default function QueueScreen() {
   return (
-    <MainLayout>
+    <MainLayout showHeader={false}>
+      <Header />
       <Queue />
     </MainLayout>
   );
