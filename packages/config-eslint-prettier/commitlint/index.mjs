459c7382e0c152f9649f13b6ad239d72fc6f3dbf
tkhwang <tkhwang@gmail.com>
2025-10-04 19:52:42 +0900

    chore(config): remove header-max-length rule from commitlint configuration

--- packages/config-eslint-prettier/commitlint/index.mjs
+++ packages/config-eslint-prettier/commitlint/index.mjs
@@ -15,8 +15,7 @@ const TYPE_ENUM = [
 const config = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', TYPE_ENUM],
-    'header-max-length': [2, 'always', 100]
+    'type-enum': [2, 'always', TYPE_ENUM]
   }
 };
 
